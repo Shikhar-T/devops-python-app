f38dc00338d351450e0792d7ca0b4955c34301fc
Ubuntu <ubuntu@ip-172-31-45-107.ap-south-1.compute.internal>
2026-04-25 14:46:59 +0000

    Updated Flask app response

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello DevOps 🚀"
-    return "Hello FINAL CI/CD  🚀"
+
+@app.route('/test')
+def test():
+    return "Hello FINAL CI/CD 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
